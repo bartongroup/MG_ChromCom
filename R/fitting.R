4145c91ac0a805b9325451748ec8ae04a0c148a9
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2018-01-25 08:05:16 +0000

    Modifed to take 3 or 4 free parameters

--- R/fitting.R
+++ R/fitting.R
@@ -1,4 +1,4 @@
-# Rscript fitting.Rs set name switch ncells ntry t0
+# Rscript fitting.Rs set name switch ncells ntry t0 npars
 
 setwd("/cluster/gjb_lab/mgierlinski/projects/chromcomR/doc")
 source("../../mylib/R/lib.R")
@@ -12,21 +12,17 @@ library(parallel)
 library(methods)
 
 args <- commandArgs(TRUE)
-stopifnot(length(args) >= 3)
+stopifnot(length(args) == 6)
 set <- args[1]
 name <- args[2]
 t2ref <- as.integer(args[3])
 stopifnot(!is.null(dataFile[[set]]))
 stopifnot(t2ref %in% c(0, 1))
 
-ncells <- 1000
-if(!is.null(args[4])) ncells <- args[4]
-
-ntry <- 100
-if(!is.null(args[5])) ntry <- args[5]
-
-t0 <- 0
-if(!is.null(args[6])) t0 <- as.numeric(args[6])
+ncells <- args[4]
+ntry <- args[5]
+t0 <- as.numeric(args[6])
+npars <- as.integer(args[7])
 
 print(paste("Fitting", set))
 
@@ -36,12 +32,20 @@ str(echr)
 pars <- c3pars(
   t0 = t0,
   tau1 = 15,
-  tau2 = 10,
+  tau2 = 8,
   k1 = 0.05,
   k2 = 0.06,
   t2ref = t2ref
 )
 str(pars)
 
-free <- c("tau1", "k1", "k2", "tau2")
+
+if(npars == 4) {
+  free <- c("tau1", "k1", "k2", "tau2")
+} else if(npars == 3) {
+  free <- c("tau1", "k1", "k2")
+} else {
+  stop("Wrong number of parameters")
+}
+
 chr <- cacheData(name, fitChr, echr, pars, free, ncells=ncells, ntry=ntry, ncores=8, binDir=binDir)
